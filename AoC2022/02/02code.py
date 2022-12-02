dfa4c988eaff15796bf2d2f98b1d7151538de6db
gvfredriksson <gvfredriksson@gmail.com>
2022-12-02 06:55:33 +0100

    Brute force 02

---
+++ AoC2022/02/02code.py
@@ -0,0 +1,81 @@
+def read_data():
+    data = []
+    f = open("C:/Users/GvFre/Github/Advent_of_code/AoC2022/02/02data", "r")
+    for row in f:
+        data.append(row.split("\n")[0].strip(","))
+    data[0] = data[0].strip("[")
+    data[-1] = data[-1].strip("]")
+    return data
+
+def part1():
+    #a = ["A Y", "B X", "C Z"]
+    a = read_data()
+    score = 0
+    for i in a:
+        if i[2] == "X":
+            score += 1
+            if i[0] == "A":
+                score += 3
+            if i[0] == "C":
+                score += 6
+        if i[2] == "Y":
+            score += 2
+            if i[0] == "B":
+                score += 3
+            if i[0] == "A":
+                score += 6
+        if i[2] == "Z":
+            score += 3
+            if i[0] == "C":
+                score += 3
+            if i[0] == "B":
+                score += 6
+    print(score)
+
+
+def part2():
+    #X = lose
+    #Y = Draw
+    #Z = Win
+    #a = ["A Y", "B X", "C Z"]
+    win_sheet = {
+        "A": "Y",
+        "B": "Z",
+        "C": "X",
+    }
+    lose_sheet = {
+        "A": "Z",
+        "B": "X",
+        "C": "Y",
+    }
+    a = read_data()
+    score = 0
+    for i in a:
+        if i[2] == "X":
+            if i[0] == "A":
+                score += 3
+            if i[0] == "B":
+                score += 1
+            if i[0] == "C":
+                score += 2
+        if i[2] == "Y":
+            score += 3
+            if i[0] == "A":
+                score += 1
+            if i[0] == "B":
+                score += 2
+            if i[0] == "C":
+                score += 3
+        if i[2] == "Z":
+            score += 6
+            if i[0] == "A":
+                score += 2
+            if i[0] == "B":
+                score += 3
+            if i[0] == "C":
+                score += 1
+    print(score)
+
+part1()
+print("---------------------")
+part2()
