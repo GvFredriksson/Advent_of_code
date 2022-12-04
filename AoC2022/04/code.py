eafe05b0421b4d6f685b87d86970b127ac5b9ed0
gvfredriksson <gvfredriksson@gmail.com>
2022-12-04 07:28:21 +0100

    04

--- AoC2022/04/code.py
+++ AoC2022/04/code.py
@@ -14,15 +14,33 @@ def read_data(name):
 
 
 def part1():
-    a = read_data("data_test")
-    #a = read_data("data")
-
-
+    #a = read_data("data_test")
+    a = read_data("data")
+    sum = 0
+    for i in a:
+        b = i.split(",")
+        c = b[0].split("-")
+        d = b[1].split("-")
+        if int(c[0]) >= int(d[0]) and int(c[1]) <= int(d[1]):
+            sum += 1
+        elif int(c[0]) <= int(d[0]) and int(c[1]) >= int(d[1]):
+            sum += 1
+    print(sum)
 
 
 def part2():
-    a = read_data("data_test")
-    #a = read_data("data")
+    #a = read_data("data_test")
+    a = read_data("data")
+    sum = 0
+    for i in a:
+        b = i.split(",")
+        c = b[0].split("-")
+        d = b[1].split("-")
+        if int(c[0]) >= int(d[0]) and int(c[0]) <= int(d[1]) or int(c[1]) >= int(d[0]) and int(c[1]) <= int(d[1]):
+            sum += 1
+        elif int(c[0]) <= int(d[0]) and int(c[1]) >= int(d[0]) or int(c[0]) <= int(d[1]) and int(c[1]) >= int(d[1]):
+            sum += 1
+    print(sum)
 
 
 
