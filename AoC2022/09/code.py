897dd002810221b51e800287e720e3477ffb21dc
gvfredriksson <gvfredriksson@gmail.com>
2022-12-09 06:57:17 +0100

    09 v1

--- AoC2022/09/code.py
+++ AoC2022/09/code.py
@@ -16,12 +16,87 @@ def read_data(name):
 def part1():
     #a = read_data("data_test")
     a = read_data("data")
+    H = [0,0]
+    T = [0,0]
+    ans = [[0,0]]
+    for i in a:
+        sub_i = i.split(" ")
+        dir = sub_i[0]
+        for _ in range(0,int(sub_i[1])):
+            if dir == "R":
+                H[0] += 1
+            if dir == "L":
+                H[0] -= 1
+            if dir == "U":
+                H[1] += 1
+            if dir == "D":
+                H[1] -= 1
+            if H[0] > T[0]+1 and H[1] == T[1]:
+                T[0] += 1
+            elif H[0] < T[0]-1 and H[1] == T[1]:
+                T[0] -= 1
+            elif H[1] > T[1]+1 and H[0] == T[0]:
+                T[1] += 1
+            elif H[1] < T[1]-1 and H[0] == T[0]:
+                T[1] -= 1
+            elif H[0] > T[0]+1 and H[1] > T[1] or H[0] > T[0] and H[1] > T[1]+1:
+                T[0] += 1
+                T[1] += 1
+            elif H[0] > T[0]+1 and H[1] < T[1] or H[0] > T[0] and H[1] < T[1]-1:
+                T[0] += 1
+                T[1] -= 1
+            elif H[0] < T[0]-1 and H[1] > T[1] or H[0] < T[0] and H[1] > T[1]+1:
+                T[0] -= 1
+                T[1] += 1
+            elif H[0] < T[0]-1 and H[1] < T[1] or H[0] < T[0] and H[1] < T[1]-1:
+                T[0] -= 1
+                T[1] -= 1
+            if T not in ans:
+                ans.append([T[0],T[1]])
+    print(len(ans))
 
 
 def part2():
     #a = read_data("data_test")
     a = read_data("data")
-
+    T = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+    ans = [[0,0]]
+    for i in a:
+        sub_i = i.split(" ")
+        dir = sub_i[0]
+        for _ in range(0,int(sub_i[1])):
+            if dir == "R":
+                T[0][0] += 1
+            if dir == "L":
+                T[0][0] -= 1
+            if dir == "U":
+                T[0][1] += 1
+            if dir == "D":
+                T[0][1] -= 1
+            for j in range(1,len(T)):
+                if T[j-1][0] > T[j][0]+1 and T[j-1][1] == T[j][1]:
+                    T[j][0] += 1
+                elif T[j-1][0] < T[j][0]-1 and T[j-1][1] == T[j][1]:
+                    T[j][0] -= 1
+                elif T[j-1][1] > T[j][1]+1 and T[j-1][0] == T[j][0]:
+                    T[j][1] += 1
+                elif T[j-1][1] < T[j][1]-1 and T[j-1][0] == T[j][0]:
+                    T[j][1] -= 1
+                elif T[j-1][0] > T[j][0]+1 and T[j-1][1] > T[j][1] or T[j-1][0] > T[j][0] and T[j-1][1] > T[j][1]+1:
+                    T[j][0] += 1
+                    T[j][1] += 1
+                elif T[j-1][0] > T[j][0]+1 and T[j-1][1] < T[j][1] or T[j-1][0] > T[j][0] and T[j-1][1] < T[j][1]-1:
+                    T[j][0] += 1
+                    T[j][1] -= 1
+                elif T[j-1][0] < T[j][0]-1 and T[j-1][1] > T[j][1] or T[j-1][0] < T[j][0] and T[j-1][1] > T[j][1]+1:
+                    T[j][0] -= 1
+                    T[j][1] += 1
+                elif T[j-1][0] < T[j][0]-1 and T[j-1][1] < T[j][1] or T[j-1][0] < T[j][0] and T[j-1][1] < T[j][1]-1:
+                    T[j][0] -= 1
+                    T[j][1] -= 1
+            if T[9] not in ans:
+                ans.append([T[9][0],T[9][1]])
+    print(len(ans))
 
 
 part1()
